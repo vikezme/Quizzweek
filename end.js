8926c4bb51debd2b14249aba94f20ba9b5583d98
kumar vikrant <vikezme@gmail.com>
2021-07-23 11:32:31 +0930

    adding file

--- end.js
+++ end.js
@@ -1,5 +1,5 @@
 let name = sessionStorage.getItem ("name");
-let points = sessionStorage.getItem ("points");
+let points = sessionStorage.getItem ("Points");
 
 document.querySelector('.name').innerHTML= name;
 document.querySelector('.points').innerHTML= points;
